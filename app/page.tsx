2b317d58ed1d72f9dcdf5bb88f5bc83145cec2d6
Rabudev <raulraposo24082001@gmail.com>
2025-12-29 17:00:49 +0100

    instalcion y configuracion de shdcn/ui , button de la libria

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
    <>
-     
+     <div>
+           <Button>Click me</Button>
+     </div>
    </>
   );
 }
